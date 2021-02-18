e94adb2f30357c1115e8605764c3f9cacbcacc46
yusptar <yusupvatar>
2021-02-18 14:32:22 +0700

    update project

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # hello_world
 <p>1. Hasil Screenshot Tugas Pertama Flutter<p>
-https://user-images.githubusercontent.com/70737618/108281242-f9430080-71b1-11eb-8c95-a54cf873296e.jpg)<br>
-https://user-images.githubusercontent.com/70737618/108281399-445d1380-71b2-11eb-9abc-2206e1af7eaa.jpg)<br>
+![1](https://user-images.githubusercontent.com/70737618/108281242-f9430080-71b1-11eb-8c95-a54cf873296e.jpg)<br>
+![2](https://user-images.githubusercontent.com/70737618/108281399-445d1380-71b2-11eb-9abc-2206e1af7eaa.jpg)<br>
 <p>2. Hasil Screenshot Tugas Kedua Flutter<P>
+![1](https://user-images.githubusercontent.com/70737618/108320610-9d01d000-71f5-11eb-859e-66c903ea80ca.jpg)<br>
 
 A new Flutter project.
 
--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
             Row(children: <Widget>[
               Container(
                 child: CupertinoButton(
+                  alignment: Alignment.center,
                   child: Text(
                     'BERITA TERBARU',
                     style: TextStyle(color: Colors.black, fontSize: 15),
@@ -26,6 +27,7 @@ class MyApp extends StatelessWidget {
                 ),
               ),
               Container(
+                alignment: Alignment.center,
                 child: CupertinoButton(
                   child: Text(
                     'PERTANDINGAN HARI INI',
@@ -35,26 +37,255 @@ class MyApp extends StatelessWidget {
                 ),
               ),
             ]),
-            Column(children: <Widget>[
-              Container(
-                alignment: Alignment.topCenter,
-                decoration: BoxDecoration(
-                  color: Colors.purpleAccent,
-                  image: const DecorationImage(
-                    image: NetworkImage(
-                        'https://asset.kompas.com/crops/RSbGxdISfzfpo_PuxQVjWCDRvjo=/1x0:511x340/750x500/data/photo/2021/02/07/601ed2bf8cfe3.jpg'),
-                    fit: BoxFit.fitWidth,
-                  ),
-                  border: Border.all(
-                    color: Colors.purpleAccent,
-                    width: 1,
+            Stack(
+              children: <Widget>[
+                Container(
+                    color: Colors.deepPurple[300],
+                    alignment: Alignment.bottomLeft,
+                    child: CupertinoButton(
+                        child: Text("Transfer",
+                            style:
+                                TextStyle(fontSize: 15, color: Colors.black)),
+                        onPressed: () {}),
+                    height: 320.0,
+                    width: 500.0,
+                    margin: EdgeInsets.all(1)),
+                Container(
+                    color: Colors.white,
+                    alignment: Alignment.bottomCenter,
+                    child: Text("Cristiano Ronaldo Sehat Wal Afi'at",
+                        style: TextStyle(fontSize: 20, color: Colors.black)),
+                    height: 280.0,
+                    width: 400.0,
+                    margin: EdgeInsets.all(3)),
+                Container(
+                    child: Image(
+                      image: NetworkImage(
+                          'https://asset.kompas.com/crops/RSbGxdISfzfpo_PuxQVjWCDRvjo=/1x0:511x340/750x500/data/photo/2021/02/07/601ed2bf8cfe3.jpg'),
+                    ),
+                    margin: EdgeInsets.all(3)),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Container(
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.black)),
+                  margin: EdgeInsets.fromLTRB(3, 0, 37, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: Image(
+                          image: NetworkImage(
+                              'https://asset-a.grid.id/crop/0x0:0x0/700x465/photo/2020/11/25/3726765514.jpg'),
+                        ),
+                        height: 100,
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                        alignment: Alignment.center,
+                        child: Text(
+                            "Valentino Rossi Akan Berhenti Balapan Tanpa Kembali ke Yamaha.",
+                            style: TextStyle(color: Colors.black)),
+                        height: 100,
+                        width: 150,
+                      ),
+                    ],
                   ),
                 ),
-                height: 200,
-                width: 300,
-                margin: EdgeInsets.all(1),
-              ),
-            ]),
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black)),
+                          alignment: Alignment.centerLeft,
+                          child: Text("Urbino Feb 18, 2021 .",
+                              style: TextStyle(color: Colors.black)),
+                          height: 40,
+                          width: 320,
+                          margin: EdgeInsets.fromLTRB(3, 0, 3, 6)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Container(
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.black)),
+                  margin: EdgeInsets.fromLTRB(3, 0, 37, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: Image(
+                          image: NetworkImage(
+                              'https://asset-a.grid.id/crop/0x0:0x0/700x465/photo/2020/11/25/3726765514.jpg'),
+                        ),
+                        height: 100,
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                        alignment: Alignment.center,
+                        child: Text(
+                            "Valentino Rossi Akan Berhenti Balapan Tanpa Kembali ke Yamaha.",
+                            style: TextStyle(color: Colors.black)),
+                        height: 100,
+                        width: 150,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black)),
+                          alignment: Alignment.centerLeft,
+                          child: Text("Urbino Feb 18, 2021 .",
+                              style: TextStyle(color: Colors.black)),
+                          height: 40,
+                          width: 320,
+                          margin: EdgeInsets.fromLTRB(3, 0, 3, 6)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Container(
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.black)),
+                  margin: EdgeInsets.fromLTRB(3, 0, 37, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: Image(
+                          image: NetworkImage(
+                              'https://asset-a.grid.id/crop/0x0:0x0/700x465/photo/2020/11/25/3726765514.jpg'),
+                        ),
+                        height: 100,
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                        alignment: Alignment.center,
+                        child: Text(
+                            "Valentino Rossi Akan Berhenti Balapan Tanpa Kembali ke Yamaha.",
+                            style: TextStyle(color: Colors.black)),
+                        height: 100,
+                        width: 150,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black)),
+                          alignment: Alignment.centerLeft,
+                          child: Text("Urbino Feb 18, 2021 .",
+                              style: TextStyle(color: Colors.black)),
+                          height: 40,
+                          width: 320,
+                          margin: EdgeInsets.fromLTRB(3, 0, 3, 6)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Container(
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.black)),
+                  margin: EdgeInsets.fromLTRB(3, 0, 37, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: Image(
+                          image: NetworkImage(
+                              'https://asset-a.grid.id/crop/0x0:0x0/700x465/photo/2020/11/25/3726765514.jpg'),
+                        ),
+                        height: 100,
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                        alignment: Alignment.center,
+                        child: Text(
+                            "Valentino Rossi Akan Berhenti Balapan Tanpa Kembali ke Yamaha.",
+                            style: TextStyle(color: Colors.black)),
+                        height: 100,
+                        width: 150,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black)),
+                          alignment: Alignment.centerLeft,
+                          child: Text("Urbino Feb 18, 2021 .",
+                              style: TextStyle(color: Colors.black)),
+                          height: 40,
+                          width: 320,
+                          margin: EdgeInsets.fromLTRB(3, 0, 3, 6)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Container(
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.black)),
+                  margin: EdgeInsets.fromLTRB(3, 0, 37, 0),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        child: Image(
+                          image: NetworkImage(
+                              'https://asset-a.grid.id/crop/0x0:0x0/700x465/photo/2020/11/25/3726765514.jpg'),
+                        ),
+                        height: 100,
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                        alignment: Alignment.center,
+                        child: Text(
+                            "Valentino Rossi Akan Berhenti Balapan Tanpa Kembali ke Yamaha.",
+                            style: TextStyle(color: Colors.black)),
+                        height: 100,
+                        width: 150,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black)),
+                          alignment: Alignment.centerLeft,
+                          child: Text("Urbino Feb 18, 2021 .",
+                              style: TextStyle(color: Colors.black)),
+                          height: 40,
+                          width: 320,
+                          margin: EdgeInsets.fromLTRB(3, 0, 3, 6)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
